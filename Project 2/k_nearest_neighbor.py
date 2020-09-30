9d50cb83bf85206daeae379119c8ba37b593bcf4
crichardswa <crichards.wa@gmail.com>
2020-09-30 16:50:58 -0600

    Added Pre-Process info to markdown file

--- Project 2/k_nearest_neighbor.py
+++ Project 2/k_nearest_neighbor.py
@@ -15,17 +15,25 @@ import dataset_class
 
 
 class k_near_neighbor:
+    # ----
+    # FUNCTION TO INITALIZE KNN OBJECT
+    # ----
     def __init__(self, k, data_obj):
         self.k = k
         self.regression = data_obj.regression
         self.trainArr = data_obj.trainArr
-        self.testArr = data_obj.testArr
-        
+        self.testArr = data_obj.testArr  
     
+    # ----
+    # FUNCTION TO DEFINE WHAT THE TRAIN AND TEST SETS ARE TO USE
+    # ----
     def fit(self, train, test):
         self.train = train
         self.test = test
-        
+               
+    # ----
+    # FUNCTION TO PREDICT THE CLASS OF A ROW FROM THE TEST SET
+    # ----   
     def predict(self, testRow):
         print("--- Predict Class by KNN ---")
         distances = []
@@ -45,8 +53,18 @@ class k_near_neighbor:
         
         # Predict Class
         topNeighbors['Class'].value_counts()
-
     
+    # ----
+    # FUNCTION TO RUN THE KNN ALGORITHM
+    # ----   
+    def run_knn(self):
+        ## Run for each 10 fold cross
+        for i in range(10): #Run for each set, 10 times
+            fit(self.trainArr[i], self.testArr[i]) #Define train and test data sets
+ 
+# ----
+# FUNCTION TO FIND THE EUCLIDEAN DISTANCE BETWEEN ALL VALUES BETWEEN TWO ROWS
+# ---- 
 def euclidean_distance(row1, row2):
     distance = 0.0
     for i in range(len(row1)-1):

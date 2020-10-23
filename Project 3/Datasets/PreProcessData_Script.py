6ff84e2e8d6a6ccfc0c316cc4b8819728b45172a
crichardswa <crichards.wa@gmail.com>
2020-10-22 18:55:01 -0600

    Added Main File, Added Script File for Pre-Process Data

---
+++ Project 3/Datasets/PreProcessData_Script.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+CSCI 447 - Machine Learning
+Project Three
+
+Pre-Process Data Script
+
+@author: Claire Richards, Olivia Firth, Hannah C
+"""
+
+import pandas as pd
---
+++ Project 3/Project3_Main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+CSCI 447 - Machine Learning
+Project Three
+
+@author: Claire Richards, Olivia Firth, Hannah C
+"""
+

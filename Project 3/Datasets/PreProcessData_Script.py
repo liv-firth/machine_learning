762c1e60ba529290a8fe615844ba34e06e6dfa6b
crichardswa <crichards.wa@gmail.com>
2020-10-23 13:27:18 -0600

    Dataset files

--- Project 3/Datasets/PreProcessData_Script.py
+++ Project 3/Datasets/PreProcessData_Script.py
@@ -9,3 +9,11 @@ Pre-Process Data Script
 """
 
 import pandas as pd
+
+
+## ABALONE DATA SPECIFIC ACTIONS
+    # CODIFY SEX COLUMN TO THE FOLLOWING: m = 1, w = 2, i = 3
+
+## BREAST CANCER SPECIFIC ACTIONS
+    # 16 MISSING VALUES (DENOTED BY ?): RANDOMLY ASSIGN VALUES (1-10)
+    

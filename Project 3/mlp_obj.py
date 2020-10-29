3c43769942d27f0ebc12cdc0931e604844f19422
crichardswa <crichards.wa@gmail.com>
2020-10-29 01:20:41 -0600

    MLP setup laid out (using reference)

--- Project 3/mlp_obj.py
+++ Project 3/mlp_obj.py
@@ -94,8 +94,40 @@ class mlp:
             if i != len(network)-1:
                 for j in range(len(layer)):
                     error = 0.0
-                    for neuron in network[i+1]
-        
+                    for neuron in network[i+1]:
+                        error += (neuron['weights'][j]*neuron['delta'])
+                    errors.append(error)
+            else:
+                for j in range(len(layer)):
+                    neuron = layer[j]
+                    errors.append(expected[j] - neuron['output'])
+            for j in range(len(layer)):
+                neuron = layer[j]
+                neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+    # Update network weights with error
+    def update_weights(self, network, row, l_rate):
+    	for i in range(len(network)):
+    		inputs = row[:-1]
+    		if i != 0:
+    			inputs = [neuron['output'] for neuron in network[i - 1]]
+    		for neuron in network[i]:
+    			for j in range(len(inputs)):
+    				neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+    			neuron['weights'][-1] += l_rate * neuron['delta']
+     
+    # Train a network for a fixed number of epochs
+    def train_network(self, network, train, l_rate, n_epoch, n_outputs):
+    	for epoch in range(n_epoch):
+    		sum_error = 0
+    		for row in train:
+    			outputs = forward_propagate(network, row)
+    			expected = [0 for i in range(n_outputs)]
+    			expected[row[-1]] = 1
+    			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
+    			backward_propagate_error(network, expected)
+    			update_weights(network, row, l_rate)
+    		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+            
     # ----
     # RUN FUNCTION
     # ----
